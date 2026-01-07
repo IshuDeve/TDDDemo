d81910a5ae8f11a6f309b83cf22deb905c75284e
IshuDeve <guptaayushi664@gmail.com>
2026-01-07 10:20:08 +0530

    test case for 0 string

--- test/string_calci_test.dart
+++ test/string_calci_test.dart
@@ -1,9 +1,14 @@
 
 
+import 'package:flutter_test/flutter_test.dart';
 import 'package:testdriven/StringCalci.dart';
 
 void main (){
   final cal = Stringcalci();
+
+  test('returns 0 for empty string', () {
+    expect(cal.add(''), equals(0));
+  });
 
 
 }
